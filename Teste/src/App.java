b31f166d86f3ebf0cdfb11be0e5a66283d20ae85
fausto-bernardo <fausto.bernardo@gmail.com>
2022-02-24 20:02:46 -0300

    adicionando projeto Teste

---
+++ Teste/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void  main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String simbolos = " => <= ++ !== -> <- === =/= != = --> <--";
+        /* Comentário errado  */
+        System.out.println(simbolos);
+    }
+}
